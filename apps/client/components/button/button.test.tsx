1dd09e81c36db1fc11b839f5db940380846f9c4d
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2024-03-04 02:48:21 -0300

    refactor files structure

--- apps/client/components/button/button.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Button from '@/components/button/index'
-
-describe('button', () => {
-  it('should render component', () => {
-    render(<Button>Test</Button>)
-    expect(screen.getByRole('button', { name: 'Test' })).toBeInTheDocument()
-  })
-})
