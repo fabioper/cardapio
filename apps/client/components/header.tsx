266435d82155639795b3014f5899df385b51ffaa
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2024-03-04 21:45:40 -0300

    add header icons

--- apps/client/components/header.tsx
+++ apps/client/components/header.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Link from 'next/link'
+import { TbSearch, TbShoppingBag } from 'react-icons/tb'
+import { HiMiniBars3 } from 'react-icons/hi2'
+import { MdOutlineAccountCircle } from 'react-icons/md'
 
 function Header() {
   return (
     <header className="py-5 bg-white border-b border-border">
-      <div className="container flex place-content-center">
-        <Link href="/" className="text-lg font-medium">
+      <div className="container flex place-content-between">
+        <Link href="/" className="font-bold">
           Cardápio
         </Link>
+
+        <div className="flex place-content-center gap-5 text-2xl">
+          <TbSearch />
+          <MdOutlineAccountCircle />
+          <TbShoppingBag />
+          <HiMiniBars3 />
+        </div>
       </div>
     </header>
   )
