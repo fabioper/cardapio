8d127d9c235db415296914a39d3969df43b8f82f
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2024-03-06 03:03:43 -0300

    update products style

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -6,10 +6,12 @@ export default async function Home() {
   const products = await getProducts()
 
   return (
-    <main className="py-3">
-      <h2 className="text-xl font-bold mb-3 container">Produtos</h2>
+    <main className="py-5 lg:py-10">
+      <h2 className="text-2xl lg:text-4xl font-extrabold mb-3 container">
+        Produtos
+      </h2>
 
-      <section className="container flex flex-col lg:grid lg:grid-cols-3 lg:gap-4">
+      <section className="container flex flex-col lg:grid lg:grid-cols-4 lg:gap-4">
         {products.map(product => (
           <ProductCard key={product.id} product={product} />
         ))}
