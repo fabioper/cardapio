8f632a6687888eff7255831f85a7ce29cdb23449
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2024-03-10 02:08:58 -0300

    add overflow auto to action bar

--- apps/client/components/action-bar/action-bar.tsx
+++ apps/client/components/action-bar/action-bar.tsx
@@ -6,7 +6,7 @@ import Portal from '@/components/portal'
 export default function ActionBar({ children }: PropsWithChildren) {
   return (
     <Portal elementId="action-bar">
-      <div className="bg-surface-a py-3 border-t border-t-surface-b">
+      <div className="bg-surface-a py-3 border-t border-t-surface-b overflow-x-auto">
         {children}
       </div>
     </Portal>
