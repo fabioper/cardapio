2fc1de251d06fd698b9f10bc531ef8f59ffc02fc
Fábio Pereira da Silva <fabiopereira.fps@gmail.com>
2024-03-25 02:36:41 -0300

    update layout of delivery and payment forms

--- apps/client/app/checkout/entrega/delivery-form.tsx
+++ apps/client/app/checkout/entrega/delivery-form.tsx
@@ -17,14 +17,40 @@ export default function DeliveryForm() {
 
   return (
     <form className="flex flex-col gap-5">
-      <div className="flex flex-col gap-1">
-        <label htmlFor="name">Endereço</label>
-        <input type="text" id="name" />
+      <div className="grid md:grid-cols-[2fr_1fr] gap-5">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="name">Endereço</label>
+          <input type="text" id="name" />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label htmlFor="number">Número</label>
+          <input type="text" id="number" />
+        </div>
       </div>
 
-      <div className="flex flex-col gap-1">
-        <label htmlFor="number">Número</label>
-        <input type="text" id="number" />
+      <div className="grid md:grid-cols-2 gap-5">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="district">Bairro</label>
+          <input type="text" id="district" />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label htmlFor="city">Cidade</label>
+          <input type="text" id="city" />
+        </div>
+      </div>
+
+      <div className="grid md:grid-cols-[1fr_2fr] gap-5">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="cep">CEP</label>
+          <input type="text" id="cep" />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label htmlFor="complement">Complemento</label>
+          <input type="text" id="complement" />
+        </div>
       </div>
 
       {smallScreen ? (
--- apps/client/app/checkout/pagamento/payment-form.tsx
+++ apps/client/app/checkout/pagamento/payment-form.tsx
@@ -1,32 +1,56 @@
 'use client'
 
-import { ActionBar, Button } from '@cardapio/ui/components'
+import { ActionBar, Button, RadioButton } from '@cardapio/ui/components'
 import React from 'react'
 import { useMediaQuery } from 'usehooks-ts'
 import clsx from 'clsx'
+import Link from 'next/link'
+import { FaCreditCard, FaMoneyBill, FaPix, FaTicket } from 'react-icons/fa6'
 
 export default function PaymentForm() {
   const smallScreen = useMediaQuery('(max-width: 500px)')
 
   const button = (
-    <Button label="Continuar" className={clsx({ 'w-full': smallScreen })} />
+    <Link href="/checkout/resumo">
+      <Button
+        label="Revisar pedido"
+        className={clsx({ 'w-full': smallScreen })}
+      />
+    </Link>
   )
 
   return (
-    <form className="flex flex-col gap-5 items-start">
-      <div className="flex items-center gap-2">
-        <input type="radio" name="payment" id="cash" />
-        <label htmlFor="cash">Dinheiro</label>
-      </div>
+    <form className="flex flex-col gap-10 items-start">
+      <div className="flex flex-col gap-5 items-start">
+        <div className="flex items-center gap-2">
+          <RadioButton name="payment" id="pix" />
+          <label htmlFor="pix" className="flex items-center gap-2">
+            <FaPix className="text-surface-e" /> Pix
+          </label>
+        </div>
 
-      <div className="flex items-center gap-2">
-        <input type="radio" name="payment" id="card" />
-        <label htmlFor="card">Cartão de crédito/débito</label>
-      </div>
+        <div className="flex items-center gap-2">
+          <RadioButton name="payment" id="cash" />
+          <label htmlFor="cash" className="flex items-center gap-2">
+            <FaMoneyBill className="text-surface-e" /> Dinheiro
+          </label>
+        </div>
 
-      <div className="flex items-center gap-2">
-        <input type="radio" name="payment" id="ticket" />
-        <label htmlFor="ticket">Ticket refeição</label>
+        <div className="flex items-center gap-2">
+          <RadioButton name="payment" id="card" />
+          <label htmlFor="card" className="flex items-center gap-2">
+            <FaCreditCard className="text-surface-e" />
+            Cartão de crédito/débito
+          </label>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <RadioButton name="payment" id="ticket" />
+          <label htmlFor="ticket" className="flex items-center gap-2">
+            <FaTicket className="text-surface-e" />
+            Ticket refeição
+          </label>
+        </div>
       </div>
 
       {smallScreen ? (
